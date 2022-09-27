35bc651849000d61568d2db7f224852d75c433c9
markoter <markoterdev@outlook.com>
2022-09-27 17:54:48 +0200

    update way to show time

--- src/App.js
+++ src/App.js
@@ -1,49 +1,73 @@
 import { useState } from "react";
 
 function App() {
-  //state
-  const [breakLen, changeBreakLen] = useState(5)
-  const [sessionLen, changeSessionLen] = useState(25)
 
+
+  //state
+  const [breakLen, changeBreakLen] = useState(300)
+  const [sessionLen, changeSessionLen] = useState(1500)
+
+  // const counter = setInterval(() => {
+  //   changeSessionLen(sessionLen -1)
+  // }, 1000)
+
+  //buttons onClicks
+  const countDown = () => {
+    console.log("countDown clicked")
+    // counter()
+  }
   const sessionInc = () => {
-    if (sessionLen < 60) {
-      changeSessionLen(sessionLen + 1)
+    if (sessionLen < 3600) {
+      changeSessionLen(sessionLen + 60)
     }
   }
   const sessionDec = () => {
     if (sessionLen > 0) {
-      changeSessionLen(sessionLen - 1)
+      changeSessionLen(sessionLen - 60)
     }
   }
   const breakInc = () => {
-    if (breakLen < 60) {
-      changeBreakLen(breakLen + 1)
+    if (breakLen < 3600) {
+      changeBreakLen(breakLen + 60)
     }
   }
   const breakDec = () => {
     if (breakLen > 0) {
-      changeBreakLen(breakLen - 1)
+      changeBreakLen(breakLen - 60)
     }
   }
   const reset = () => {
     changeBreakLen(5)
     changeSessionLen(25)
   }
+
+  //formating as time
+  const returnMinutes = (time) => {
+    return Math.floor(time / 60)
+  }
+
+  const showTime = (time) => {
+    const minutes = returnMinutes(time)
+    const seconds = time - minutes * 60
+    const showMinutes = minutes.toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false })
+    const showSeconds = seconds.toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false })
+    return minutes + ':' + showSeconds
+  }
   return (
     <div id="app">
       This is app
       <div id="timer-label">
         Session
-        <time id="time-left">{sessionLen}:00</time>
+        <time id="time-left">{showTime(sessionLen)}</time>
         <div id="controls">
-          <button id="start_stop">start/stop</button>
+          <button id="start_stop" onClick={countDown}>start/stop</button>
           <button id="reset" onClick={reset}>reset</button>
         </div>
       </div>
       <div id="inc-dec">
         <div id="session-label" className="labels">
           Session Length
-          <p id="session-length">{sessionLen}</p>
+          <p id="session-length">{returnMinutes(sessionLen)}</p>
           <div id="session-controls" className="controls">
             <button id="session-decrement" onClick={sessionDec}>Session Dec</button>
             <button id="session-increment" onClick={sessionInc}>Session Inc</button>
@@ -51,7 +75,7 @@ function App() {
         </div>
         <div id="break-label" className="labels">
           Break Length
-          <p id="break-length">{breakLen}</p>
+          <p id="break-length">{returnMinutes(breakLen)}</p>
           <div id="break-controls" className="controls">
             <button id="break-decrement" onClick={breakDec}>Break Dec</button>
             <button id="break-increment" onClick={breakInc}>Break INc</button>
