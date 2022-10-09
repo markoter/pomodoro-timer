0adad948e84703249d1a8b7b3c9dc53197fd5a85
markoter <markoterdev@outlook.com>
2022-10-09 14:07:10 +0200

    create component as in tutorial

---
+++ src/circularProgressBar.js
@@ -0,0 +1,20 @@
+const CircularProgressBar = (props) => {
+    const { ResizeObserverSize, strokeWidth } = props
+    const viewBox = `0 0 ${size} ${size}`
+    const radius = (size - strokeWidth) / 2
+
+    return (
+        <svg width={size} height={size} viewBox={viewBox}>
+            <circle
+                fill="none"
+                stroke="#ccc"
+                cx={size / 2}
+                cy={size / 2}
+                r={radius}
+                strokeWidth={`${strokeWidth}px`}
+            />
+        </svg>
+    )
+
+}
+export default CircularProgressBar
